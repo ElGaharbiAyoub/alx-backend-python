c8a567efd95f99c4825bcaada69dd71fe427fac6
elgharbi <elgharbiay@gmail.com>
2024-01-16 01:49:40 +0100

    FIX

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,6 +4,7 @@ import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 async def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
     """ spawn wait_random with the specified max_delay."""
     return await asyncio.create_task(wait_n(n, max_delay))
