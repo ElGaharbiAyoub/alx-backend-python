497b185b2cf76eee529e1da54471676cb0a154e3
elgharbi <elgharbiay@gmail.com>
2024-02-05 19:17:46 +0100

    FIX

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,9 +6,13 @@ from utils import *
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class"""
     @parameterized.expand([
-        ({"a": {"b": {"c": 1}}}, ["a", "b", "c"], 1),
-        ({"x": {"y": {"z": 2}}}, ["x", "y", "z"], 2),
-    ])
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
     def test_access_nested_map(self, nested_map, map_path, expected_output):
-        self.assertEqual(access_nested_map(nested_map, map_path), expected_output)
+        """Test access_nested_map"""
+        self.assertEqual(access_nested_map(
+            nested_map, map_path), expected_output)
