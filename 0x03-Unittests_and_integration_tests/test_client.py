a17d962467c642d4254a91c4014e537134ad3d63
elgharbi <elgharbiay@gmail.com>
2024-02-06 00:45:29 +0100

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(
             f"https://api.github.com/orgs/{org}"
         )
+
+    @parameterized.expand([
+        ("google", "repos_url", "http://google.com"),
+        ("abc", "repos_url", "http://abc.com"),
+    ])
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, org, property_name, expected, mock_org):
+        """Test public repos url"""
+        mock_org.return_value = {property_name: expected}
+        test_class = GithubOrgClient(org)
+        self.assertEqual(test_class._public_repos_url, expected)
+        mock_org.assert_called_once_with()
